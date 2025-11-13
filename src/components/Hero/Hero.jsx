c7172930c431ad81f9cc73c3a603bc35f399975f
Bohdan <yashbon@ukr.net>
2025-11-13 09:22:54 +0200

    fix Hero`s background,

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,10 +4,10 @@ const Hero = () => {
         <section id="main" className={css.hero}>
             <div className="container">
                 <div className={css.description}>
-                    <h1 className={css.title}>
-                        Ласкаво просимо до{" "}
-                        <span className={css.title_eglamed}>EglaMed</span>
-                    </h1>
+                    <h1 className={css.mainTitle}>Гінекологічна клініка EglaMed — консультації, УЗД, лікування | Житомир</h1>
+                    <span className={css.title}>
+                        Ласкаво просимо до <span className={css.title_eglamed}>EglaMed</span>
+                    </span>
                 </div>
             </div>
         </section>
