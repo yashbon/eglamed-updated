202a2f613a75503346abb40585ab664b5efff131
Bohdan <yashbon@ukr.net>
2026-05-01 23:26:20 +0300

    fix: update GTM strategy

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -55,9 +55,13 @@ export default function RootLayout({ children }) {
              >
                 <Script
                     src="https://www.googletagmanager.com/gtag/js?id=G-0VE2L3CVCD"
-                    strategy="afterInteractive"
+                    // strategy="afterInteractive"
+                    strategy="lazyOnload"
                 />
-                <Script id="google-analytics" strategy="afterInteractive">
+                <Script id="google-analytics" 
+                    // strategy="afterInteractive"
+                    strategy="lazyOnload"
+                    >
                     {`
                         window.dataLayer = window.dataLayer || [];
                         function gtag(){dataLayer.push(arguments);}
